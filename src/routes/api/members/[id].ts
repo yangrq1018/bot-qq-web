0c30d4563558c0abe3fec4834a303eb7357e1d07
yangrq-linux <martinyangrq@outlook.com>
2022-06-26 17:45:00 +0800

    use dotenv

--- src/routes/api/members/[id].ts
+++ src/routes/api/members/[id].ts
@@ -1,8 +1,9 @@
 import type { RequestEvent } from '@sveltejs/kit';
 import axios from 'axios';
+import 'dotenv/config'
 
 export async function get(event: RequestEvent) {
-    const res = await axios.get("http://127.0.0.1:8083/members/" + event.params.id)
+    const res = await axios.get(process.env.BOT_BACKEND + "/members/" + event.params.id);
     return {
         status: 200,
         body: res.data
